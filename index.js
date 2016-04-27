ef66c5e808b2f30db048390ab1acf882c66250fd
Richard Warren <rwarren92@hotmail.com>
2016-04-27 15:57:47 +0100

    Start

---
+++ index.js
@@ -0,0 +1,20 @@
+function readBox() {
+	// Take input values
+	var num1Val = document.getElementById('num1').value;
+	var num2Val = document.getElementById('num2').value;
+  var operator = document.getElementById('operator').value.toString();
+
+  var answer = 0
+
+  if (operator === "add") {
+    answer = num1Val + num2Val;
+  } else if (operator === "subtract") {
+    answer = num1Val - num2Val;
+  } else if (operator === "multiply") {
+    answer = num1Val * num2Val;
+  } else {
+    answer = num1Val / num2Val;
+  }
+
+  document.getElementById("demo").innerHTML = answer;
+}
