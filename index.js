a4f9717b673509b0646f8024716766aeeea188b4
Richard Warren <rwarren92@hotmail.com>
2016-04-27 20:22:54 +0100

    css

--- index.js
+++ index.js
@@ -18,3 +18,32 @@ function readBox() {
 
   document.getElementById("demo").innerHTML = answer;
 }
+
+function day() {
+  var myElement = document.querySelector("body");
+  myElement.style.backgroundColor = "#D93600";
+  myElement.style.color = "black";
+}
+
+function night() {
+  var myElement = document.querySelector("body");
+  myElement.style.backgroundColor = "#262626";
+  myElement.style.color = "#D93600";
+};
+
+function acidRun() {
+      var color1 = "#"+((1<<24)*Math.random()|0).toString(16)
+      var color2 = "#"+((1<<24)*Math.random()|0).toString(16)
+      var colour = Math.random()
+      var myElement = document.querySelector("body");
+      myElement.style.backgroundColor = color1;
+      myElement.style.color = color2;
+    };
+
+function acidButton() {
+  var id = setInterval(acidRun, 250);
+};
+
+function acidStop() {
+  var id = clearInterval(id);
+};
